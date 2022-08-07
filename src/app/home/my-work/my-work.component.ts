a3bb7fc6e8caf4352059cabbb4e115f807a44216
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-08-07 09:54:50 +0100

    my porject filter function and active btn

--- src/app/home/my-work/my-work.component.ts
+++ src/app/home/my-work/my-work.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyWorkComponent implements OnInit {
 
+  projects = [
+    {
+      name: 'Join',
+      description: 'A simple Task Manager',
+      img: './../../../assets/img/project_dummy.jpg',
+      url: 'https://join.kevin-schimke.de/board.html',
+      projecttype: 'JavaScript'
+    },
+    {
+      name: 'El-Pollo-Loco',
+      description: '',
+      img: './../../../assets/img/project_dummy.jpg',
+      url: 'https://join.kevin-schimke.de/board.html',
+      projecttype: 'JavaScript'
+    },
+    {
+      name: 'QuizApp',
+      description: '',
+      img: './../../../assets/img/project_dummy.jpg',
+      url: 'https://join.kevin-schimke.de/board.html',
+      projecttype: 'JavaScript'
+    },
+    {
+      name: 'Pokedex',
+      description: '',
+      img: './../../../assets/img/project_dummy.jpg',
+      url: 'https://join.kevin-schimke.de/board.html',
+      projecttype: 'JavaScript'
+    },
+    {
+      name: 'Portfolio',
+      description: '',
+      img: './../../../assets/img/project_dummy.jpg',
+      url: 'https://join.kevin-schimke.de/board.html',
+      projecttype: 'Angular'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  filtered_projects = this.projects;
+  active_btn = 'all'
+
+  filterProjects(filterName: string) {
+    if (filterName == 'all') {
+      this.filtered_projects = this.projects;
+      this.active_btn = 'all';
+    } else if (filterName == 'angular') {
+      this.filtered_projects = this.projects.filter(project => project.projecttype == 'Angular');
+      this.active_btn = 'angular'
+    } else if (filterName == 'javaScript') {
+      this.filtered_projects = this.projects.filter(project => project.projecttype == 'JavaScript');
+      this.active_btn = 'javaScript';
+    }
+  }
 }
+
+// window.open('https://join.kevin-schimke.de/board.html','_blank')
